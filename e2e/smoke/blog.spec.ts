70d5a6aed5b77ffbe2ab417e528d14251ff4f502
vinialveslopesanjos <vinialveslopesanjos@users.noreply.github.com>
2026-06-30 14:03:27 -0300

    Harden automatic deploy and signup conversion

--- e2e/smoke/blog.spec.ts
+++ e2e/smoke/blog.spec.ts
@@ -3,13 +3,17 @@ import { expect, test } from "@playwright/test";
 test.describe("Public blog", () => {
   test("blog list, article and CTA are navigable", async ({ page }) => {
     await page.goto("/blog");
-    await expect(page.getByRole("heading", { name: /Reputacao digital/i })).toBeVisible();
+    await expect(page.getByRole("heading", { name: /Reputa[cç][aã]o digital/i })).toBeVisible();
 
     await page.getByRole("link", { name: /Ler artigo/i }).first().click();
     await expect(page).toHaveURL(/\/blog\/.+/);
     await expect(page.getByRole("heading").first()).toBeVisible();
     await expect(page.getByRole("link", { name: /Voltar para o blog/i })).toBeVisible();
-    await expect(page.getByRole("link", { name: /Fazer um diagnostico|Ver como ficaria|Comecar pelo diagnostico/i })).toBeVisible();
+    await expect(
+      page.getByRole("link", {
+        name: /Fazer um diagn[oó]stico|Ver como ficaria|Come[cç]ar pelo diagn[oó]stico/i,
+      }),
+    ).toBeVisible();
 
     await page.getByRole("link", { name: /Voltar para o blog/i }).click();
     await expect(page).toHaveURL(/\/blog$/);
